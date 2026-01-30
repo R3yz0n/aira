660c3802d70c7f1880884b37d1679f680365071f
r3yz0n <reyzon.thapa2020@gmail.com>
2026-01-30 20:37:51 +0545

    fix: revert default token expiration time to 1 hour in signToken function

--- lib/auth/jwt.ts
+++ lib/auth/jwt.ts
@@ -18,7 +18,7 @@ const JWT_SECRET: jwt.Secret = secret;
 
 export function signToken(
   payload: string | object | Buffer,
-  expiresIn: jwt.SignOptions["expiresIn"] = "30h",
+  expiresIn: jwt.SignOptions["expiresIn"] = "1h",
 ) {
   return jwt.sign(payload, JWT_SECRET, { expiresIn } as jwt.SignOptions);
 }
